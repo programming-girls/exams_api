3a34765ac6027bb39d2197e5eefe900ad70e890f
Awinja-j <ingari61@hotmail.com>
2021-11-23 00:45:24 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from manage import app, db
+
+from src.exam.models.model import Exam, Subject, Question, SubQuestion, Answer, Image
+
+#blueprints
+from src.orc_engine.ocr_server import ocr_core, ocr
+from src.exam.views.text_marking_scheme import text_marking_scheme
+from src.exam.views.choice_marking_scheme import choice_marking_scheme
+from src.users.views.admin_views import ad
+
+
+
+#register blueprints
+app.register_blueprint(ocr)
+app.register_blueprint(text_marking_scheme)
+app.register_blueprint(choice_marking_scheme)
+app.register_blueprint(ad)
+
+
+with app.app_context():
+    from src.exam.models.model import Exam, Subject, Question, SubQuestion, Answer, Image
+    db.create_all()
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ manage.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
